53810f050fddb02134ae19c78daa09ceb8040718
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-07-24 14:56:51 -0500

    Made test file to test functions in core

--- test_GasPump.py
+++ test_GasPump.py
@@ -2,4 +2,14 @@ from gascore import *
 
 def test_choices():
     assert choices('1') == 'Refilled Tanks'
+
+def test_gasname():
     assert gas_name('3') == 'Premium'
+
+def test_refill():
+    assert refill([['1', 'Regular', 1, 2.13]]) == 'Num, Type, Gallons, Price\n1, Regular, 5000.00, 2.13'
+
+def test_revenue():
+    assert revenue([['1', 'Regular', 1, 2.13]]) == 2.13
+    assert revenue([['2', 'Midgrade', 2, 4.46],['1', 'Regular', 1, 2.13]]) == 6.59
+
