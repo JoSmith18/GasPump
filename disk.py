fdc8e64adfd075258ec28ed944172e9c32ac7cb3
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-09-18 13:13:06 -0500

    Combined the two mains

--- disk.py
+++ disk.py
@@ -1,28 +1,47 @@
 from gascore import *
 
+
 def load_inventory():
-    with open('tank.txt','r') as tank:
+    with open('tank.txt', 'r') as tank:
         tank.readline()
         items = []
         for line in tank.readlines():
             pieces = line.split(', ')
-            items.append([pieces[0], pieces[1], float(pieces[2]), float(pieces[3])])
+            items.append(
+                [pieces[0], pieces[1],
+                 float(pieces[2]),
+                 float(pieces[3])])
     return items
 
+
 def open_tank(message):
-    with open("tank.txt",'w') as tank:
+    with open("tank.txt", 'w') as tank:
         tank.write(message)
 
-def logit(code,amount,payment):
-    with open("log.txt",'a') as file:
+
+def logit(code, amount, payment):
+    with open("log.txt", 'a') as file:
         if payment == '1' or payment == '2':
-            file.write('{0}, {1}, {2}, {3:.2f}\n'.format(code,gas_name(code),amount[0],amount[1]))
+            file.write('{0}, {1}, {2}, {3:.2f}\n'.format(
+                code, gas_name(code), amount[0], amount[1]))
+
 
 def open_log():
-    with open("log.txt","r") as file:
+    with open("log.txt", "r") as file:
         file.readline()
         lst = file.readlines()
         text = []
         for line in lst:
             text.append(line.split())
     return text
+
+
+def write_log():
+    with open("adminlog.txt", "a") as log:
+        log.write(
+            "{},{}, {}\n".format(name, choices(choice), datetime.today()))
+
+
+def read_tank():
+    with open("tank.txt", "r") as tank:
+        print(tank.read())
---
+++ shell.py
@@ -0,0 +1,115 @@
+from gascore import *
+from datetime import *
+from disk import *
+
+
+def valid_pay():
+
+    msg = """ How Will You Pay:
+    \t1. Prepay
+    \t2. Pay After
+    """
+    while True:
+        payment = input(msg)
+        if payment == '1' or payment == '2':
+            return (payment)
+        else:
+            print('Error Try Again')
+
+
+def addup(payment, code):
+    if payment == '1':
+        money = int(input('How Much You Buying?\n'))
+        amount = add_prepay(money, code, menu)
+    elif payment == '2':
+        gallons = int(input('How Much Was Pumped?\n'))
+        amount = add_payafter(code, menu, gallons)
+    return amount
+
+
+def user_main():
+    payment = valid_pay()
+
+    selection = """ Select Code Of Gas Type:
+        \t1. Regular: 2.08
+        \t2. Midgrade: 2.23
+        \t3. Premium: 2.36
+    """
+    code = input(selection)
+
+    amount = addup(payment, code)
+
+    logit(code, amount, payment)
+
+    inventory = load_inventory()
+
+    message = write_message(inventory, code, amount)
+
+    open_tank(message)
+
+
+def admin_main():
+    code = '1234'
+    while True:
+        password = input("What is the password?\n")
+        if password == code:
+
+            inventory = load_inventory()
+
+            name = input("Hi What Is Your Name\n")
+
+            print('Hello {} You Have Clearance\n'.format(name))
+
+            decision = """ Admin Decisions
+            \t1. Refill The Tanks
+            \t2. Check The Revenue
+            \t3. Open Log
+            \t4. Look At Tanks
+            """
+
+            choice = input(decision)
+
+            if choice.strip() == '1'.strip():
+                message = refill(inventory)
+                open_tank(message)
+                read_tank()
+                break
+
+            elif choice.strip() == '2'.strip():
+                text = open_log()
+                print(revenue(text))
+                break
+
+            elif choice.strip() == '3'.strip():
+                with open("log.txt", "r") as file:
+                    print(file.read())
+                    break
+
+            elif choice.strip() == '4'.strip():
+                with open("tank.txt", "r") as tanks:
+                    print(tanks.read())
+                    break
+
+            else:
+                print("Error we are starting over")
+        else:
+            print("Invalid Password!!\n")
+
+    write_log()
+
+
+def main():
+    while True:
+        choice = input("Are you admin or user?\n").title()
+        if choice == "Admin":
+            admin_main()
+            break
+        elif choice == "User":
+            user_main()
+            break
+        else:
+            print("Invalid Choice!!!\n")
+
+
+if __name__ == '__main__':
+    main()
